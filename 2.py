95930200b4f5467bd9688e663e4deba48f37d81b
Codersintu <skmahto9199@gmail.com>
2025-09-03 19:06:01 +0530

    push

---
+++ 2.py
@@ -0,0 +1,16 @@
+# find space 
+name="hey  sintu kaise ho"
+print(name.find("  "))
+print(name.replace("  "," ")) #string are Immutable which means that you cannot change them
+# by running function on them
+
+# new concept
+letter="Dear Harry,\n\this python course is nice.\nthanks"
+print(letter)
+
+
+
+
+
+
+
---
+++ list.py
@@ -0,0 +1,5 @@
+frinds=["akash","sintu",[1,2,3,4,"hello"],["gaddha"]]
+print(frinds[0])
+frinds[0]="solooo"#list are mutable
+frinds.append("bey")
+print(frinds)
