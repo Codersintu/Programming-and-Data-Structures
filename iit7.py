4a9b95cb96960faabe07662c1e9d5e251912ed34
Codersintu <skmahto9199@gmail.com>
2025-09-18 16:09:46 +0530

    iit8.py

--- iit7.py
+++ iit7.py
@@ -1,1 +1,19 @@
 # 16/09/25
+# Balanced Binary Tree: A binary tree which have of most one different b/w subtree
+# height for each node in the tree
+
+# complete Binary tree;it have all levels full of node,except the last level,which can also be full or filled from left to right 
+
+# full binary tree: a kind of tree where each node has either 0 or 2child node
+
+# binary tree-traversal:going through a tree by visiting every day node,one node at a time is called traversal
+# (1)BFS-Breath first search==in BFS on the same level are visited before going
+# to  the next level in the tree
+
+# (2)DFS-defth first search==in this traversal moves down the tree all the way 
+# dfs three types
+# (1)pre-order
+# (2)in-order
+# (3)post-order
+
+# next class discussion iit8.py-------------------------------
---
+++ iit8.py
@@ -0,0 +1,55 @@
+# 17/09/2025
+# Inorder traversal
+# Inorder traversal visits the node in the order:left to root to right
+# class Node:
+#     def __init__(self,value):
+#         self.value = value
+#         self.right=None
+#         self.left=None
+
+# def inorder(root):
+#         if root is not None:
+#             inorder(root.left)
+#             print(root.value,end=" ")
+#             inorder(root.right)
+
+
+# root=Node("A")
+# root.left=Node('B')
+# root.right=Node('c')
+# root.left.left=Node("D")
+# root.left.right=Node("E")
+# root.right.left=Node("F")
+# root.right.right=Node("G")
+
+# inorder(root)
+
+
+
+# Preorder Traversal
+
+# class Node:
+#     def __init__(self,value):
+#         self.value = value
+#         self.right=None
+#         self.left=None
+
+# def preorder(root):
+#         if root is not None:
+#             print(root.value,end=" ")
+#             preorder(root.left)
+#             preorder(root.right)
+
+
+# root=Node("A")
+# root.left=Node('B')
+# root.right=Node('c')
+# root.left.left=Node("D")
+# root.left.right=Node("E")
+# root.right.left=Node("F")
+# root.right.right=Node("G")
+
+# preorder(root)
+
+
+# postorder Traversal
