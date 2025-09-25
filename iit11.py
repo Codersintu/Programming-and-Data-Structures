3d608328eea229c95076bdd0bca5f06728820899
Codersintu <skmahto9199@gmail.com>
2025-09-25 19:27:28 +0530

    today done iit11.py classe

--- iit11.py
+++ iit11.py
@@ -2,3 +2,11 @@
 # AVL Tree Implementation in Python
 # A type of binarysearch tree
 # AVL tree is a self-balancing binary search tree
+# Balance-factor=a nodes balance factor is the different in subtree height
+# Balance factor = height(left subtree) - height(right subtree)
+# balance factor values=
+# 0-the node is perfectly balanced
+# >0-the node is right heavy
+# <0-the node is left heavy
+# if BF(x)>1-x is right heavy and unbalanced
+# if BF(x)<-1-x is left heavy and unbalanced
